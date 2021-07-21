afb26665db96ce596c7abb5f58b063e47910bfe4
James Dinsdale <hi@molovo.co>
2021-07-21 09:28:02 +0100

    Setup layout and default sass

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,25 @@
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+const React = require("react")
+const Layout = require("./src/components/layout").default
+
+exports.wrapPageElement = ({ element, props }) => (
+  <Layout {...props}>{element}</Layout>
+)
+
+ exports.onRouteUpdate = ({ location: { pathname }, prevLocation }) => {
+  if (pathname === "/") {
+    pathname = "home"
+  }
+
+  document.body.setAttribute(
+    "class",
+    `page ${[
+      ...pathname
+        .split("/")
+        .filter(x => !!x)
+        .map(slug => `page--${slug}`),
+    ].join(" ")}`
+  )
+}
