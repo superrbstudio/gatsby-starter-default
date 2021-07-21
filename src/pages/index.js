cda96a7934a050ef842aa1ae4ca0507d8e106c01
James Dinsdale <james@superrb.com>
2021-07-21 10:20:21 +0100

    Remove layout from index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <Seo title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
@@ -23,7 +23,7 @@ const IndexPage = () => (
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
-  </Layout>
+  </>
 )
 
 export default IndexPage
